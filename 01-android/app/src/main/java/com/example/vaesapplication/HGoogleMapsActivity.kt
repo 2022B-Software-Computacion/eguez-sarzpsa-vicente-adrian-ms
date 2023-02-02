87fc0c2eba668a184e543b1f461e77ae45aae474
Adrian Eguez <eadepto@hotmail.com>
2023-02-02 10:56:33 -0500

    google maps funcionando

--- 01-android/app/src/main/java/com/example/vaesapplication/HGoogleMapsActivity.kt
+++ 01-android/app/src/main/java/com/example/vaesapplication/HGoogleMapsActivity.kt
@@ -3,13 +3,13 @@ package com.example.vaesapplication
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
 class HGoogleMapsActivity : AppCompatActivity() {
     private lateinit var mapa: GoogleMap
@@ -29,6 +29,47 @@ class HGoogleMapsActivity : AppCompatActivity() {
             if (googleMap != null) {
                 mapa = googleMap
                 establecerConfiguracionMapa()
+                val zoom = 17f
+                val quicentro = LatLng(
+                    -0.17556708490271092, -78.48014901143776
+                )
+                val titulo = "Quicentro"
+                val markQuicentro = anadirMarcador(quicentro, titulo)
+                markQuicentro.tag = titulo
+
+                val poliLineaUno = googleMap
+                    .addPolyline(
+                        PolylineOptions()
+                            .clickable(true)
+                            .add(
+                                LatLng(-0.1759187040647396,
+                                    -78.48506472421384),
+                                LatLng(-0.17632468492901104,
+                                    -78.48265589308046),
+                                LatLng(-0.17746143130181483,
+                                    -78.4770533307815)
+                            )
+                    )
+                poliLineaUno.tag = "linea-1" // <- ID
+
+                // POLIGONO
+                val poligonoUno = googleMap
+                    .addPolygon(
+                        PolygonOptions()
+                            .clickable(true)
+                            .add(
+                                LatLng(-0.1771546902239471,
+                                    -78.48344981495214),
+                                LatLng(-0.17968981486125768,
+                                    -78.48269198043828),
+                                LatLng(-0.17710958124147777,
+                                    -78.48142892291516)
+                            )
+                    )
+                poligonoUno.fillColor = -0xc771c4
+                poligonoUno.tag = "poligono-2" // <- ID
+                escucharListeners()
+
             }
         }
     }
@@ -50,12 +91,39 @@ class HGoogleMapsActivity : AppCompatActivity() {
 
     }
 
-    fun anadirMarcador(latLng: LatLng, title: String){
-        mapa.addMarker(
+
+    fun escucharListeners(){
+        mapa.setOnPolygonClickListener {
+            Log.i("mapa", "setOnPolygonClickListener ${it}")
+            it.tag // ID
+        }
+        mapa.setOnPolylineClickListener {
+            Log.i("mapa", "setOnPolylineClickListener ${it}")
+            it.tag // ID
+        }
+        mapa.setOnMarkerClickListener {
+            Log.i("mapa", "setOnMarkerClickListener ${it}")
+            it.tag // ID
+            return@setOnMarkerClickListener true
+        }
+        mapa.setOnCameraMoveListener {
+            Log.i("mapa", "setOnCameraMoveListener")
+        }
+        mapa.setOnCameraMoveStartedListener {
+            Log.i("mapa", "setOnCameraMoveStartedListener ${it}")
+        }
+        mapa.setOnCameraIdleListener {
+            Log.i("mapa", "setOnCameraIdleListener")
+        }
+    }
+
+
+    fun anadirMarcador(latLng: LatLng, title: String): Marker {
+        return mapa.addMarker(
             MarkerOptions()
                 .position(latLng)
                 .title(title)
-        )
+        )!!
     }
 
     fun moverCamaraConZoom(latLng: LatLng, zoom: Float = 10f){
